80080cca031033df4217778d257cb9dae846a99c
youssef <youssef@lex-persona.com>
2020-01-15 11:49:02 +0100

    Rock it

---
+++ main.js
@@ -0,0 +1,58 @@
+//get the element where we will include our shadow dom
+const shadow_element = document.getElementById('twitter_link_container')
+
+/*  attachShadow: method attaches a shadow DOM tree to the specified element and returns a reference to its ShadowRoot.
+    mode: a string specifying the encapsulation mode for the shadow DOM tree. This can be one of:
+        - open: Elements of the shadow root are accessible from JavaScript outside the root
+        - closed: Denies access to the node(s) of a closed shadow root from JavaScript
+    
+    [source]=https://developer.mozilla.org/en-US/docs/Web/API/Element/attachShadow#Syntax
+*/
+const shadow = shadow_element.attachShadow({ mode: 'open' })
+
+//Create the Twitter link
+const twitter_link = document.createElement('span');
+twitter_link.innerHTML = ` 
+    <a href="https://twitter.com/youssame0">
+    <span class="icon"></span> Follow @youssame0
+    </a>
+`;
+//add the Twitter link to the shadow dom
+shadow.appendChild(twitter_link);
+
+//Create the Twitter link style
+const style = document.createElement('style');
+style.textContent = `
+a, span {
+  vertical-align: top;
+  display: inline-block;
+  box-sizing: border-box;
+}
+
+a {
+    height: 20px;
+    padding: 1px 8px 1px 6px;
+    background-color: #1b95e0;
+    color: #fff;
+    border-radius: 3px;
+    font-weight: 500;
+    font-size: 11px;
+    font-family:'Helvetica Neue', Arial, sans-serif;
+    line-height: 18px;
+    text-decoration: none;   
+}
+
+a:hover {  background-color: #0c7abf; }
+
+span.icon {
+    position: relative;
+    top: 2px;
+    width: 14px;
+    height: 14px;
+    margin-right: 3px;
+    background: transparent 0 0 no-repeat;
+    background-image: url(data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2072%2072%22%3E%3Cpath%20fill%3D%22none%22%20d%3D%22M0%200h72v72H0z%22%2F%3E%3Cpath%20class%3D%22icon%22%20fill%3D%22%23fff%22%20d%3D%22M68.812%2015.14c-2.348%201.04-4.87%201.744-7.52%202.06%202.704-1.62%204.78-4.186%205.757-7.243-2.53%201.5-5.33%202.592-8.314%203.176C56.35%2010.59%2052.948%209%2049.182%209c-7.23%200-13.092%205.86-13.092%2013.093%200%201.026.118%202.02.338%202.98C25.543%2024.527%2015.9%2019.318%209.44%2011.396c-1.125%201.936-1.77%204.184-1.77%206.58%200%204.543%202.312%208.552%205.824%2010.9-2.146-.07-4.165-.658-5.93-1.64-.002.056-.002.11-.002.163%200%206.345%204.513%2011.638%2010.504%2012.84-1.1.298-2.256.457-3.45.457-.845%200-1.666-.078-2.464-.23%201.667%205.2%206.5%208.985%2012.23%209.09-4.482%203.51-10.13%205.605-16.26%205.605-1.055%200-2.096-.06-3.122-.184%205.794%203.717%2012.676%205.882%2020.067%205.882%2024.083%200%2037.25-19.95%2037.25-37.25%200-.565-.013-1.133-.038-1.693%202.558-1.847%204.778-4.15%206.532-6.774z%22%2F%3E%3C%2Fsvg%3E);
+}
+`;
+//add the Twitter link style to the shadow dom
+shadow.append(style);
